c2115f8df0c3c9fb815ce0b231162d3df43b72ca
Matheus Leal <109047505+Leall675@users.noreply.github.com>
2025-08-22 10:58:26 -0300

    fix: ajustes no user service

--- src/main/java/com/leal/users_api/application/UserServiceImpl.java
+++ src/main/java/com/leal/users_api/application/UserServiceImpl.java
@@ -4,8 +4,11 @@ import com.leal.users_api.application.dto.UserDto;
 import com.leal.users_api.application.dto.UserDtoResponse;
 import com.leal.users_api.domain.User;
 import com.leal.users_api.domain.UserRepository;
-import com.leal.users_api.infrastructure.Mapper;
+import com.leal.users_api.infrastructure.UserRepositoryAdapter;
+import com.leal.users_api.infrastructure.config.Mapper;
+import com.leal.users_api.infrastructure.config.security.SecurityConfig;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -16,11 +19,13 @@ import java.util.List;
 public class UserServiceImpl implements UserService{
 
     private final Mapper mapper;
-    private final UserRepository userRepository;
+    private final UserRepositoryAdapter userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public UserDtoResponse register(UserDto userDto) {
         User user = mapper.toEntity(userDto);
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         User userSaved = userRepository.save(user);
         return mapper.toDto(userSaved);
     }
