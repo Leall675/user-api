faeb48dbaece59f34b61195b78831453ca6e6442
Matheus Leal <109047505+Leall675@users.noreply.github.com>
2025-08-22 08:09:38 -0300

    feat: implementacao do controller para login cadastro e buscar de usuarios

--- src/main/java/com/leal/users_api/web/UserController.java
+++ src/main/java/com/leal/users_api/web/UserController.java
@@ -1,4 +1,35 @@
 package com.leal.users_api.web;
 
+import com.leal.users_api.application.UserService;
+import com.leal.users_api.application.dto.UserDto;
+import com.leal.users_api.application.dto.UserDtoResponse;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/users")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public ResponseEntity<UserDtoResponse> register(@Valid @RequestBody UserDto userDto) {
+        UserDtoResponse response = userService.register(userDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserDtoResponse>> getAllUsers() {
+        List<UserDtoResponse> responses = userService.findAll();
+        if (responses.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(responses);
+    }
 }
