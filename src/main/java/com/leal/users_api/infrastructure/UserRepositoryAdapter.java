d5a6602e321739af05feef450de6185041119b9e
Matheus Leal <109047505+Leall675@users.noreply.github.com>
2025-08-22 10:59:58 -0300

    feat: User repository adapter

--- src/main/java/com/leal/users_api/infrastructure/UserRepositoryAdapter.java
+++ src/main/java/com/leal/users_api/infrastructure/UserRepositoryAdapter.java
@@ -1,4 +1,28 @@
 package com.leal.users_api.infrastructure;
 
+import com.leal.users_api.domain.User;
+import com.leal.users_api.domain.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
 public class UserRepositoryAdapter {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public Optional<User> findByCpf(String cpf) {
+        return userRepository.findByCpf(cpf);
+    }
 }
