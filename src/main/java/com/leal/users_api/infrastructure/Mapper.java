35285c97f781b14ecd8ee20e7a0c04876fe76fd2
Matheus Leal <109047505+Leall675@users.noreply.github.com>
2025-08-21 21:33:54 -0300

    feat: objectMapper para tratar objetos

--- src/main/java/com/leal/users_api/infrastructure/Mapper.java
+++ src/main/java/com/leal/users_api/infrastructure/Mapper.java
@@ -1,4 +1,23 @@
 package com.leal.users_api.infrastructure;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.leal.users_api.application.dto.UserDto;
+import com.leal.users_api.application.dto.UserDtoResponse;
+import com.leal.users_api.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Mapper {
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    public User toEntity(UserDto userDto) {
+        return objectMapper.convertValue(userDto, User.class);
+    }
+
+    public UserDtoResponse toDto(User user) {
+        return objectMapper.convertValue(user, UserDtoResponse.class);
+    }
+
 }
