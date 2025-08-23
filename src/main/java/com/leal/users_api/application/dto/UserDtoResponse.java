f3f02d9168876a4f5b68066c368214a183ebae3c
Matheus Leal <109047505+Leall675@users.noreply.github.com>
2025-08-23 19:07:13 -0300

    fix: ajuste no DTO de resposta para nao dar erro no campo ao desserializar

--- src/main/java/com/leal/users_api/application/dto/UserDtoResponse.java
+++ src/main/java/com/leal/users_api/application/dto/UserDtoResponse.java
@@ -1,11 +1,13 @@
 package com.leal.users_api.application.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class UserDtoResponse {
     private String id;
     private String cpf;
