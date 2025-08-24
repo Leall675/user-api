59ed496ec09c9d261a0662f3d3672c9a65c9f58e
Matheus Leal <109047505+Leall675@users.noreply.github.com>
2025-08-24 09:17:28 -0300

    feat: liberando rotas do openApi no security config

--- src/main/java/com/leal/users_api/infrastructure/config/security/SecurityConfig.java
+++ src/main/java/com/leal/users_api/infrastructure/config/security/SecurityConfig.java
@@ -37,7 +37,12 @@ public class SecurityConfig {
                 .csrf().disable()
                 .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/v1/auth/login", "api/v1/users/register").permitAll()
+                        .requestMatchers(
+                                "/api/v1/auth/login",
+                                "/api/v1/users/register",
+                                "/swagger-ui/**",
+                                "/v3/api-docs/**",
+                                "/swagger-resources/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/users").hasRole("admin")
                         .anyRequest().authenticated()
                 )
