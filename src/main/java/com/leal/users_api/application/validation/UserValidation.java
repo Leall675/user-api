4fa60984cc0bb7cd3ec1f68f8bd3624d021ea76e
Matheus Leal <109047505+Leall675@users.noreply.github.com>
2025-08-24 23:16:40 -0300

    feat: implementando validacoes

--- src/main/java/com/leal/users_api/application/validation/UserValidation.java
+++ src/main/java/com/leal/users_api/application/validation/UserValidation.java
@@ -3,10 +3,13 @@ package com.leal.users_api.application.validation;
 import com.leal.users_api.application.dto.UserDto;
 import com.leal.users_api.domain.User;
 import com.leal.users_api.domain.UserRepository;
+import com.leal.users_api.domain.enuns.RolesEnum;
 import com.leal.users_api.infrastructure.exception.UserAlreadyExistException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 
 @Component
@@ -19,6 +22,13 @@ public class UserValidation {
         Optional<User> user = userRepository.findByCpf(userDto.getCpf());
         if (user.isPresent()) {
             throw new UserAlreadyExistException("Usuário com o CPF informado já existe na base de dados.");
+        }
+        rolesDefinition(userDto);
+    }
+
+    private void rolesDefinition(UserDto userDto) {
+        if (userDto.getRoles() == null){
+            userDto.setRoles(Collections.singletonList(RolesEnum.COMMON));
         }
     }
 }
