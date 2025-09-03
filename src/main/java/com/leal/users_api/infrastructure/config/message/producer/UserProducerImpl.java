b144f1b0ce1a7398adf2ee48059003bb6ea616a1
Matheus Leal <lealdasilva2022@hotmail.com>
2025-09-03 08:55:32 -0300

    fix: package

--- src/main/java/com/leal/users_api/infrastructure/config/message/producer/UserProducerImpl.java
+++ src/main/java/com/leal/users_api/infrastructure/config/message/producer/UserProducerImpl.java
@@ -1,15 +1,10 @@
-package com.leal.users_api.infrastructure.config.message;
+package com.leal.users_api.infrastructure.config.message.producer;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.leal.users_api.application.dto.UserDto;
-import com.leal.users_api.domain.User;
 import com.leal.users_api.domain.port.out.UserEventPublisher;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 
 @Component
 @RequiredArgsConstructor
