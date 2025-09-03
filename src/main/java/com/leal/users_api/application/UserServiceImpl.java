955211ea4c645d9621896ca37895b1c20c89fe92
Matheus Leal <lealdasilva2022@hotmail.com>
2025-09-03 10:57:28 -0300

    fix: ajuste no nome da classe

--- src/main/java/com/leal/users_api/application/UserServiceImpl.java
+++ src/main/java/com/leal/users_api/application/UserServiceImpl.java
@@ -5,7 +5,7 @@ import com.leal.users_api.application.dto.UserDtoResponse;
 import com.leal.users_api.application.validation.UserValidation;
 import com.leal.users_api.domain.User;
 import com.leal.users_api.domain.port.out.UserEventPublisher;
-import com.leal.users_api.infrastructure.UserRepositoryAdapter;
+import com.leal.users_api.infrastructure.UserRepositoryImpl;
 import com.leal.users_api.infrastructure.config.Mapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -19,7 +19,7 @@ import java.util.List;
 public class UserServiceImpl implements UserService{
 
     private final Mapper mapper;
-    private final UserRepositoryAdapter userRepository;
+    private final UserRepositoryImpl userRepository;
     private final PasswordEncoder passwordEncoder;
     private final UserValidation userValidation;
     private final UserEventPublisher userEventPublisher;
--- src/main/java/com/leal/users_api/infrastructure/UserRepositoryImpl.java
+++ src/main/java/com/leal/users_api/infrastructure/UserRepositoryImpl.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public class UserRepositoryAdapter {
+public class UserRepositoryImpl {
 
     @Autowired
     private UserRepository userRepository;
